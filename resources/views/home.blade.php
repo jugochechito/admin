c3ef70276a5295d33710ce7a5b7665e031a39cff
jugochechito <sergio.rodrigo.adio@gmail.com>
2024-06-12 15:44:38 -0400

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Campaña')
+
+@section('content_header')
+    <h1>Campaña</h1>
+@stop
+
+@section('content')
+    <p>Esta es la pagina de administrador.</p>
+@stop
+
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop
